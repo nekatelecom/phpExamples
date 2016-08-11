d2df67fe2c6a4b538d8ae1fac5bbed5f2cba5f87
Mahmoud Eskandari <mahmoud-eskandari@users.noreply.github.com>
2016-08-12 01:58:06 +0430

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
     define("api_username","ApiCodehere...");
     define("api_password","Passwrd here...");
-    define("api_url","http://ws.3g4u.ir/");
+    define("api_url","https://ws.nekatelecom.com/");
