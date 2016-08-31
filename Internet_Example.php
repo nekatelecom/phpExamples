1ac2b0e862a9527c63ad1159ec93cb7204043564
Mahmoud Eskandari <mahmoud-eskandari@users.noreply.github.com>
2016-08-31 14:41:44 +0430

    Update Internet_Example.php

--- Internet_Example.php
+++ Internet_Example.php
@@ -3,4 +3,4 @@
     require './vendor/autoload.php';
     use Neka\Topup;
 
-    var_dump(Topup::buy_request('093500...',175000,'Ir',null,null,false,"33,1")); // برای مثال خرید بسته با کد33 برای سیمکارت اعتباری ایرانسل
+    var_dump(Topup::buy_request('093500...',175000,'Ir',null,null,false,"14")); // برای مثال خرید بسته با کد33 برای سیمکارت اعتباری ایرانسل
